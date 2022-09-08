c8d54fea46c763b7fabd395d8dd9b6f739f19e9f
Evgeniy Sviridov <info@myownbiz.ru>
2022-09-08 22:52:33 +0500

    add crm_type

---
+++ client/crm_type.go
@@ -0,0 +1,51 @@
+package client
+
+import "github.com/sviridoves/go-bitrix/types"
+
+func (c *Client) CrmTypeFields(data interface{}) (*types.Response, error) {
+	resp, err := c.DoRaw("crm.type.fields", data, &types.Response{})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Result().(*types.Response), err
+}
+
+func (c *Client) CrmTypeAdd(data interface{}) (*types.Response, error) {
+	resp, err := c.DoRaw("crm.type.add", data, &types.Response{})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Result().(*types.Response), err
+}
+
+func (c *Client) CrmTypeGet(data interface{}) (*types.Response, error) {
+	resp, err := c.DoRaw("crm.type.get", data, &types.Response{})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Result().(*types.Response), err
+}
+
+func (c *Client) CrmTypeList(data interface{}) (*types.CompaniesResponse, error) {
+	resp, err := c.DoRaw("crm.type.list", data, &types.CompaniesResponse{})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Result().(*types.CompaniesResponse), err
+}
+
+func (c *Client) CrmTypeUpdate(data interface{}) (*types.Response, error) {
+	resp, err := c.DoRaw("crm.type.update", data, &types.Response{})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Result().(*types.Response), err
+}
+
+func (c *Client) CrmtypeDelete(data interface{}) (*types.Response, error) {
+	resp, err := c.DoRaw("crm.type.delete", data, &types.Response{})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Result().(*types.Response), err
+}
---
+++ types/crm/type.go
@@ -0,0 +1,54 @@
+package crm
+
+type Type []struct {
+	ID                        int    `json:"ID,string"`
+	Title                     string `json:"TITLE"`
+	Code                      string `json:"CODE"`
+	CreatedBy                 int    `json:"CREATED_BY"`
+	EntityTypeID              int    `json:"ENTITY_TYPE_ID"`
+	IsCategoriesEnabled       string `json:"IS_CATEGORIES_ENABLED"`
+	IsStagesEnabled           string `json:"IS_STAGES_ENABLED"`
+	IsBeginCloseDatesEnabled  string `json:"IS_BEGIN_CLOSE_DATES_ENABLED"`
+	IsClientEnabled           string `json:"IS_CLIENT_ENABLED"`
+	IsUseInUserfieldEnabled   string `json:"IS_USE_IN_USERFIELD_ENABLED"`
+	IsLinkWithProductsEnabled string `json:"IS_LINK_WITH_PRODUCTS_ENABLED"`
+	IsMycompanyEnabled        string `json:"IS_MYCOMPANY_ENABLED"`
+	IsDocumentsEnabled        string `json:"IS_DOCUMENTS_ENABLED"`
+	IsSourceEnabled           string `json:"IS_SOURCE_ENABLED"`
+	IsObserversEnabled        string `json:"IS_OBSERVERS_ENABLED"`
+	IsRecyclebinEnabled       string `json:"IS_RECYCLEBIN_ENABLED"`
+	IsAutomationEnabled       string `json:"IS_AUTOMATION_ENABLED"`
+	IsBizProcEnabled          string `json:"IS_BIZPROC_ENABLED"`
+	IsSetOpenPermissions      string `json:"IS_SET_OPEN_PERMISSIONS"`
+	IsPaymentsEnabled         string `json:"IS_PAYMENTS_ENABLED"`
+}
+
+//type crmTypeGetStruct struct {
+//	Result struct {
+//		Type struct {
+//			Relations                 struct {
+//				Parent []struct {
+//					EntityTypeID          int    `json:"entityTypeId"`
+//					IsChildrenListEnabled string `json:"isChildrenListEnabled"`
+//					IsPredefined          string `json:"isPredefined"`
+//				} `json:"parent"`
+//				Child []struct {
+//					EntityTypeID          int    `json:"entityTypeId"`
+//					IsChildrenListEnabled string `json:"isChildrenListEnabled"`
+//					IsPredefined          string `json:"isPredefined"`
+//				} `json:"child"`
+//			} `json:"relations"`
+//			LinkedUserFields struct {
+//				CALENDAREVENTUFCRMCALEVENT string `json:"CALENDAR_EVENT|UF_CRM_CAL_EVENT"`
+//				TASKSTASKUFCRMTASK         string `json:"TASKS_TASK|UF_CRM_TASK"`
+//				TASKSTASKTEMPLATEUFCRMTASK string `json:"TASKS_TASK_TEMPLATE|UF_CRM_TASK"`
+//			} `json:"linkedUserFields"`
+//			CustomSections []struct {
+//				ID         int    `json:"id"`
+//				Title      string `json:"title"`
+//				IsSelected string `json:"isSelected"`
+//			} `json:"customSections"`
+//			CustomSectionID int `json:"customSectionId"`
+//		} `json:"type"`
+//	}
+//}
