fa212b77242e7d20386870155cd1aec17ade6c26
Evgeniy Sviridov <info@myownbiz.ru>
2022-09-28 15:13:37 +0500

    fix type.go

--- types/crm/type.go
+++ types/crm/type.go
@@ -1,31 +1,53 @@
 package crm
 
 type Type struct {
-	Types []Types `json:"types"`
+	Types []struct {
+		ID                        int    `json:"ID"`
+		Title                     string `json:"TITLE"`
+		Code                      string `json:"CODE"`
+		CreatedBy                 int    `json:"CREATED_BY"`
+		EntityTypeID              int    `json:"ENTITY_TYPE_ID"`
+		IsCategoriesEnabled       string `json:"IS_CATEGORIES_ENABLED"`
+		IsStagesEnabled           string `json:"IS_STAGES_ENABLED"`
+		IsBeginCloseDatesEnabled  string `json:"IS_BEGIN_CLOSE_DATES_ENABLED"`
+		IsClientEnabled           string `json:"IS_CLIENT_ENABLED"`
+		IsUseInUserfieldEnabled   string `json:"IS_USE_IN_USERFIELD_ENABLED"`
+		IsLinkWithProductsEnabled string `json:"IS_LINK_WITH_PRODUCTS_ENABLED"`
+		IsMycompanyEnabled        string `json:"IS_MYCOMPANY_ENABLED"`
+		IsDocumentsEnabled        string `json:"IS_DOCUMENTS_ENABLED"`
+		IsSourceEnabled           string `json:"IS_SOURCE_ENABLED"`
+		IsObserversEnabled        string `json:"IS_OBSERVERS_ENABLED"`
+		IsRecyclebinEnabled       string `json:"IS_RECYCLEBIN_ENABLED"`
+		IsAutomationEnabled       string `json:"IS_AUTOMATION_ENABLED"`
+		IsBizProcEnabled          string `json:"IS_BIZPROC_ENABLED"`
+		IsSetOpenPermissions      string `json:"IS_SET_OPEN_PERMISSIONS"`
+		IsPaymentsEnabled         string `json:"IS_PAYMENTS_ENABLED"`
+	} `json:"types"`
 }
 
-type Types struct {
-	ID                        int    `json:"ID"`
-	Title                     string `json:"TITLE"`
-	Code                      string `json:"CODE"`
-	CreatedBy                 int    `json:"CREATED_BY"`
-	EntityTypeID              int    `json:"ENTITY_TYPE_ID"`
-	IsCategoriesEnabled       string `json:"IS_CATEGORIES_ENABLED"`
-	IsStagesEnabled           string `json:"IS_STAGES_ENABLED"`
-	IsBeginCloseDatesEnabled  string `json:"IS_BEGIN_CLOSE_DATES_ENABLED"`
-	IsClientEnabled           string `json:"IS_CLIENT_ENABLED"`
-	IsUseInUserfieldEnabled   string `json:"IS_USE_IN_USERFIELD_ENABLED"`
-	IsLinkWithProductsEnabled string `json:"IS_LINK_WITH_PRODUCTS_ENABLED"`
-	IsMycompanyEnabled        string `json:"IS_MYCOMPANY_ENABLED"`
-	IsDocumentsEnabled        string `json:"IS_DOCUMENTS_ENABLED"`
-	IsSourceEnabled           string `json:"IS_SOURCE_ENABLED"`
-	IsObserversEnabled        string `json:"IS_OBSERVERS_ENABLED"`
-	IsRecyclebinEnabled       string `json:"IS_RECYCLEBIN_ENABLED"`
-	IsAutomationEnabled       string `json:"IS_AUTOMATION_ENABLED"`
-	IsBizProcEnabled          string `json:"IS_BIZPROC_ENABLED"`
-	IsSetOpenPermissions      string `json:"IS_SET_OPEN_PERMISSIONS"`
-	IsPaymentsEnabled         string `json:"IS_PAYMENTS_ENABLED"`
-}
+//
+//type Types struct {
+//	ID                        int    `json:"ID"`
+//	Title                     string `json:"TITLE"`
+//	Code                      string `json:"CODE"`
+//	CreatedBy                 int    `json:"CREATED_BY"`
+//	EntityTypeID              int    `json:"ENTITY_TYPE_ID"`
+//	IsCategoriesEnabled       string `json:"IS_CATEGORIES_ENABLED"`
+//	IsStagesEnabled           string `json:"IS_STAGES_ENABLED"`
+//	IsBeginCloseDatesEnabled  string `json:"IS_BEGIN_CLOSE_DATES_ENABLED"`
+//	IsClientEnabled           string `json:"IS_CLIENT_ENABLED"`
+//	IsUseInUserfieldEnabled   string `json:"IS_USE_IN_USERFIELD_ENABLED"`
+//	IsLinkWithProductsEnabled string `json:"IS_LINK_WITH_PRODUCTS_ENABLED"`
+//	IsMycompanyEnabled        string `json:"IS_MYCOMPANY_ENABLED"`
+//	IsDocumentsEnabled        string `json:"IS_DOCUMENTS_ENABLED"`
+//	IsSourceEnabled           string `json:"IS_SOURCE_ENABLED"`
+//	IsObserversEnabled        string `json:"IS_OBSERVERS_ENABLED"`
+//	IsRecyclebinEnabled       string `json:"IS_RECYCLEBIN_ENABLED"`
+//	IsAutomationEnabled       string `json:"IS_AUTOMATION_ENABLED"`
+//	IsBizProcEnabled          string `json:"IS_BIZPROC_ENABLED"`
+//	IsSetOpenPermissions      string `json:"IS_SET_OPEN_PERMISSIONS"`
+//	IsPaymentsEnabled         string `json:"IS_PAYMENTS_ENABLED"`
+//}
 
 //type crmTypeGetStruct struct {
 //	Result struct {
